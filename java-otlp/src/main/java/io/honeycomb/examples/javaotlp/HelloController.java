e87783f979d4f103d0eac7058785c938247dc196
Vera Reynolds <verareynolds@honeycomb.io>
2021-04-02 12:50:15 -0600

    Manual instrumentation - propagating context across process boundaries - adding baggage field to each span

--- java-otlp/src/main/java/io/honeycomb/examples/javaotlp/HelloController.java
+++ java-otlp/src/main/java/io/honeycomb/examples/javaotlp/HelloController.java
@@ -1,24 +1,64 @@
 package io.honeycomb.examples.javaotlp;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.*;
+import io.opentelemetry.api.baggage.*;
+import io.opentelemetry.api.trace.*;
+import io.opentelemetry.context.*;
+import io.opentelemetry.context.propagation.*;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.annotation.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import static io.opentelemetry.api.GlobalOpenTelemetry.*;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		Span span = GlobalOpenTelemetry.getTracer("java-otlp")
-			.spanBuilder("hello")
-			.startSpan();
-		try {
-			return "Hello world!";
-		} finally {
-			span.end();
-		}
-	}
+    @RequestMapping("/")
+    public String index(@RequestHeader Map<String, String> headers) {
+        final TextMapPropagator textMapPropagator = getPropagators().getTextMapPropagator();
+        Context ctx = textMapPropagator.extract(Context.current(), headers, new TextMapGetter<>() {
+            @Override
+            public Iterable<String> keys(Map<String, String> carrier) {
+                return carrier.keySet();
+            }
+
+            @Nullable
+            @Override
+            public String get(@Nullable Map<String, String> carrier, String key) {
+                if (carrier != null) {
+                    return carrier.get(key);
+                } else {
+                    return null;
+                }
+            }
+        });
+
+        try (Scope scope = ctx.with(Baggage.fromContext(ctx).toBuilder().put("foof", "20s").build()).makeCurrent()) {
+            Span span = GlobalOpenTelemetry.getTracer("java-otlp")
+                .spanBuilder("hello")
+                .startSpan();
+            try {
+                final URL url = new URL("http://localhost:8080/todos");
+                final HttpURLConnection transportLayer = (HttpURLConnection) url.openConnection();
+                getPropagators().getTextMapPropagator().inject(Context.current(), transportLayer, URLConnection::setRequestProperty);
+                try {
+                    transportLayer.connect();
+                    final int responseCode = transportLayer.getResponseCode();
+                    System.out.println("got response: " + responseCode);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                span.end();
+            }
+        }
+        return "Hello world!";
+    }
 
 }
--- java-otlp/src/main/java/io/honeycomb/examples/javaotlp/JavaOtlpApplication.java
+++ java-otlp/src/main/java/io/honeycomb/examples/javaotlp/JavaOtlpApplication.java
@@ -3,8 +3,11 @@ package io.honeycomb.examples.javaotlp;
 import java.util.Arrays;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.baggage.propagation.*;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.propagation.*;
+import io.opentelemetry.context.propagation.*;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
@@ -20,37 +23,43 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class JavaOtlpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaOtlpApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOtlpApplication.class, args);
 
-		// Create OTLP exporter that sends trace data to Honeycomb
-		OtlpGrpcSpanExporter exporter = OtlpGrpcSpanExporter.builder()
-			.setEndpoint("https://api.honeycomb.io:443")
-			.addHeader("x-honeycomb-team", "<YOUR-APIKEY>")
-			.addHeader("x-honeycomb-dataset", "<YOUR-DATASET>")
-			.build();
+        // Create OTLP exporter that sends trace data to Honeycomb
+        OtlpGrpcSpanExporter exporter = OtlpGrpcSpanExporter.builder()
+            .setEndpoint(System.getenv("HONEYCOMB_API_ENDPOINT"))
+            .addHeader("x-honeycomb-team", System.getenv("HONEYCOMB_API_KEY"))
+            .addHeader("x-honeycomb-dataset", System.getenv("HONEYCOMB_DATASET"))
+            .build();
+//
+        // Configure the OpenTelemtry SDK with the exporter
+        ContextPropagators propagators = ContextPropagators.create(
+            TextMapPropagator.composite(
+                W3CTraceContextPropagator.getInstance(),
+                W3CBaggagePropagator.getInstance()));
+        OpenTelemetrySdk.builder()
+            .setPropagators(propagators)
+            .setTracerProvider(
+                SdkTracerProvider.builder()
+                    .setResource(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "java-otlp")))
+                    .addSpanProcessor(new BaggageSpanProcessor())
+                    .addSpanProcessor(BatchSpanProcessor.builder(exporter).build())
+                    .build())
+            .buildAndRegisterGlobal();
+    }
 
-		// Configure the OpenTelemtry SDK with the exporter
-		OpenTelemetrySdk.builder()
-			.setTracerProvider(
-				SdkTracerProvider.builder()
-					.setResource(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "java-otlp")))
-				.addSpanProcessor(BatchSpanProcessor.builder(exporter).build())
-				.build())
-			.buildAndRegisterGlobal();
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
 
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
+        };
+    }
 
 }
